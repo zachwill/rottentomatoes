ae5c32a924ef53627145fbce1ec2a3e8d670dc11
Zach Williams <hey@zachwill.com>
2011-04-15 15:41:11 -0500

    Added new methods and refactored old ones

--- rottentomatoes.py
+++ rottentomatoes.py
@@ -38,24 +38,28 @@ class RT(object):
         if isinstance(version, float):
             version = str(version) # eliminate any weird float behavior
         self.version = version
-        self.BASE_URL = 'http://api.rottentomatoes.com/api/public/v%s/' %\
-                version
-        self.lists_url = self.BASE_URL + 'lists'
-        self.search_url = self.BASE_URL + 'movies?'
+        BASE_URL = 'http://api.rottentomatoes.com/api/public/v%s/' % version
+        self.BASE_URL = BASE_URL
+        self.lists_url = BASE_URL + 'lists'
+        self.movie_url = BASE_URL + 'movies'
 
-    def search(self, query, page=1, page_limit=30):
-        """Rotten Tomatoes movie search. Returns a list of dictionaries."""
-        params = urlencode({'apikey': self.api_key, 'q': query, 'page': page,
-            'page_limit': page_limit})
-        url = self.search_url + params
-        data = json.loads(urllib2.urlopen(url).read())
+    def search(self, query, **kwargs):
+        """
+        Rotten Tomatoes movie search. Returns a list of dictionaries.
+        Possible kwargs include: `page` and `page_limit`.
+
+        >>> RT().search('the lion king')
+        """
+        search_url = self.movie_url + '?'
+        kwargs.update({'apikey': self.api_key, 'q': query})
+        params = urlencode(kwargs)
+        search_url += params
+        data = json.loads(urllib2.urlopen(search_url).read())
         return data['movies']
 
     def lists(self, directory=None, sub=None, **kwargs):
         """
         Displays the lists available in the Rotten Tomatoes API.
-
-        Usage:
 
         >>> RT().lists()
         {u'links': {u'movies': u'http://link-to-movies'
@@ -77,12 +81,63 @@ class RT(object):
         data = json.loads(urllib2.urlopen(lists_url).read())
         return data
 
+    def info(self, id_num, specific_info=None):
+        """
+        Return info for a movie given its `id`.
+        Arguments for `specific_info` include `cast` and `reviews`.
+
+        >>> fight_club = u'13153'
+        >>> RT().info(fight_club)
+        >>> # For cast info
+        ... RT().info(fight_club, 'cast')
+        """
+        if isinstance(id_num, int):
+            id_num = str(id_num)
+        movie_url = self.movie_url
+        movie_url += '/%s' % id_num
+        if specific_info:
+            movie_url += '/%s' % specific_info
+        end_of_url = '.json?%s' % urlencode({'apikey': self.api_key})
+        movie_url += end_of_url
+        data = json.loads(urllib2.urlopen(movie_url).read())
+        return data
+
     def new(self, kind='movies', **kwargs):
         """
         Short method to return just opened theatrical movies or newly
         released dvds. Returns a list of dictionaries.
+
+        >>> RT().new('dvds', page_limit=5)
         """
         if kind == 'movies':
             return self.lists('movies', 'opening', **kwargs)['movies']
         elif kind == 'dvds':
             return self.lists('dvds', 'new_releases', **kwargs)['movies']
+
+    def movies(self, sub='in_theaters', **kwargs):
+        """
+        Short method for returning specific movie lists.
+        Possible sub aruments include: `box_office`, `in_theaters`,
+        `opening`, and `upcoming`.
+
+        >>> RT().movies('in_theaters', page_limit=5)
+        """
+        return self.lists('movies', sub, **kwargs)['movies']
+
+    def dvds(self, sub='new_releases', **kwargs):
+        """
+        Short method for returning specific movie lists.
+        Currently, only one sub argument is possible: `new_releases`.
+
+        >>> RT().dvds(page_limit=5)
+        """
+        return self.lists('dvds', sub, **kwargs)['movies']
+
+    def feeling_lucky(self, search_term):
+        """
+        Similar to Google's **I'm Feeling Lucky** button.
+        Returns first instance of search term.
+
+        >>> RT().feeling_lucky('memento')
+        """
+        return self.search(search_term, page_limit=1)[0]
