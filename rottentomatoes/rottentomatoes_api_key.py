4f89c133342c1b21d79a81e17e4581806a743bad
Andrew Lilja <andrewlilja@gmail.com>
2014-10-31 17:33:36 -0500

    restoring deleted files

---
+++ rottentomatoes/rottentomatoes_api_key.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+"""
+Rotten Tomatoes API:  http://developer.rottentomatoes.com/
+
+Store your API key in this file -- that way the `RT` class can be initialized
+without having to pass it in as an argument.
+"""
+
+API_KEY = ''
